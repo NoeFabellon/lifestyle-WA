788122363d0b3a09967115b53a9564e1e816778a
noe.fabellon.rnr <noe.fabellon.rnr@gmail.com>
2018-12-20 15:48:29 +0800

    Able to view localities

---
+++ app/assets/javascripts/localities.js
@@ -0,0 +1,32 @@
+$(document).on("turbolinks:load", () => {
+  let localities = ajax("api/localities", "", "GET");
+  localities = JSON.parse(localities.responseText).data;
+  localities.forEach(locality => {
+    if (locality.province == null) {
+      province_name = ''
+      superLocale_name = ''
+      countryName = ''
+    }
+    else {
+      province = locality.province;
+      province_name = locality.province.name
+      superLocale_name = locality.province.superLocale.name
+      countryName = locality.province.superLocale.country.countryName
+    }
+    console.log(locality)
+    $('#localities-table table tbody').append(`
+      <tr class="uk-padding-remove">
+        <td><img src='${locality.image}' class='localities-images') /></td>
+        <td>${locality.name}</td>
+        <td>${province_name}</td>
+        <td>${superLocale_name}</td>
+        <td>${countryName}</td>
+        <td>
+          <form action="" method='GET'>
+            <button type='submit' class="uk-button-primary uk-border-rounded"><span uk-icon="icon: pencil"></span></button>
+          </form>
+        </td>
+      </tr>
+    `);
+  });
+});
