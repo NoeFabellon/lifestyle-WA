c637a561fcd0a82085f96bf58d4832d8f5ebf5b7
noe.fabellon.rnr <noe.fabellon.rnr@gmail.com>
2018-12-21 11:06:46 +0800

    Able to search vouchers and invert table rows

--- app/assets/javascripts/vouchers.js
+++ app/assets/javascripts/vouchers.js
@@ -38,7 +38,7 @@ $(document).on("turbolinks:load", () => {
     }
     fullname = voucher.customer.firstName + " " + voucher.customer.lastName
     let newDateCreated = formatDate(voucher.dateCreated);
-    $('#vouchers-table table tbody').append(`
+    $('#vouchers-table tbody').append(`
       <tr class="uk-padding-remove">
         <td class="uk-text-bold">${voucher.code}</td>
         <td>${voucher.content.title}</td>
@@ -51,5 +51,53 @@ $(document).on("turbolinks:load", () => {
         <td>${status}</td>
       </tr>
     `);
+  });
+  //change dropdrown label when click dropdown option
+  $('.vouchers-filter').on("click", e => {
+    document.getElementById("vouchers-dropdown-label").innerHTML = e.currentTarget.innerHTML;
+  });
+  $('#vouchers-search-field').on("keyup", e => {
+    searchby = document.getElementById("vouchers-dropdown-label").innerHTML;
+    if (searchby == 'Search from' || searchby == 'Code') {
+      td_index = 0;
+    }
+    else if (searchby == 'Activity/Event') {
+      td_index = 1;
+    }
+    else if (searchby == 'Customer') {
+      td_index = 2;
+    }
+    else if (searchby == 'Amount') {
+      td_index = 3;
+    }
+    else if (searchby == 'Quantity') {
+      td_index = 4;
+    }
+    else if (searchby == 'Schedule') {
+      td_index = 5;
+    }
+    else if (searchby == 'Location') {
+      td_index = 6;
+    }
+    else if (searchby == 'Date Created') {
+      td_index = 7;
+    }
+    else if (searchby == 'Status') {
+      td_index = 8;
+    }
+    input = document.getElementById("vouchers-search-field");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("vouchers-table");
+    tr = table.getElementsByTagName("tr");
+    $('#vouchers-table').removeClass('uk-table-striped');
+    searchData(td_index, input, filter, table, tr);
+  });
+  //ascending descending
+  $('#vouchers-asc-dsc-button').on("click", e => {
+    $("tbody").each(function (elem, index) {
+      var arr = $.makeArray($("tr", this).detach());
+      arr.reverse();
+      $(this).append(arr);
+    });
   });
 });
