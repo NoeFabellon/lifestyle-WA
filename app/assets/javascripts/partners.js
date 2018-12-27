58bc20aadc191ba49c814426a8023435e19f9acd
noe.fabellon.rnr <noe.fabellon.rnr@gmail.com>
2018-12-27 09:32:08 +0800

    Able to search experiences and invert table rows

--- app/assets/javascripts/partners.js
+++ app/assets/javascripts/partners.js
@@ -1,27 +1,4 @@
 $(document).on("turbolinks:load", () => {
-  function formatDate(date) {
-    var monthNames = [
-      "January", "February", "March",
-      "April", "May", "June", "July",
-      "August", "September", "October",
-      "November", "December"
-    ];
-    var day = new Date(date).getDate();
-    var monthIndex = new Date(date).getMonth();
-    var year = new Date(date).getFullYear();
-    var hour = new Date(date).getHours();
-    var minute = new Date(date).getMinutes();
-    var amPM = (hour > 11) ? "PM" : "AM";
-    if (hour > 12) {
-      hour -= 12;
-    } else if (hour == 0) {
-      hour = "12";
-    }
-    if (minute < 10) {
-      minute = "0" + minute;
-    }
-    return monthNames[monthIndex] + ' ' + day + ', ' + year + " " + hour + ":" + minute + " " + amPM;
-  }
   let partners = ajax("api/partners", "", "GET");
   partners = JSON.parse(partners.responseText).data;
   partners.forEach(partner => {
