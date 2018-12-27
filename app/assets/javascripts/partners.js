a06e305ca0a3a0c57710da8b36f5a62dbe4366c3
noe.fabellon.rnr <noe.fabellon.rnr@gmail.com>
2018-12-27 09:53:43 +0800

    Able to search partners and invert table rows

--- app/assets/javascripts/partners.js
+++ app/assets/javascripts/partners.js
@@ -3,7 +3,7 @@ $(document).on("turbolinks:load", () => {
   partners = JSON.parse(partners.responseText).data;
   partners.forEach(partner => {
     let newDateCreated = formatDate(partner.dateCreated);
-    $('#partners-table table tbody').append(`
+    $('#partners-table tbody').append(`
       <tr class="uk-padding-remove">
         <td>${partner.name}</td>
         <td>${partner.name}</td>
@@ -28,5 +28,36 @@ $(document).on("turbolinks:load", () => {
         </td>
       </tr>
     `);
+  });
+  //change dropdrown label when click dropdown option
+  $('.partners-filter').on("click", e => {
+    document.getElementById("partners-dropdown-label").innerHTML = e.currentTarget.innerHTML;
+  });
+  $('#partners-search-field').on("keyup", e => {
+    searchby = document.getElementById("partners-dropdown-label").innerHTML;
+    if (searchby == 'Search from' || searchby == 'Business Name') {
+      td_index = 0;
+    }
+    else if (searchby == 'Published Business Name') {
+      td_index = 1;
+    }
+    else if (searchby == 'Partner ID') {
+      td_index = 2;
+    }
+    else if (searchby == 'Date Created') {
+      td_index = 3;
+    }
+    else if (searchby == 'App Status') {
+      td_index = 4;
+    }
+    else if (searchby == 'Display Status') {
+      td_index = 5;
+    }
+    input = document.getElementById("partners-search-field");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("partners-table");
+    tr = table.getElementsByTagName("tr");
+    $('#partners-table').removeClass('uk-table-striped');
+    searchData(td_index, input, filter, table, tr);
   });
 });
